d87f8a7cac32683fb8398867f82e3ee84dcd28fc
atToby <thoppi@student.tgm.ac.at>
2025-02-11 08:51:30 +0100

    added QuizGame & MainMenu

--- Code/src/mainMenu/MainMenuControl.java
+++ Code/src/mainMenu/MainMenuControl.java
@@ -1,7 +1,39 @@
-package mainMenu;ackage mainMenu;
+package Code.src.mainMenu;
+
+import Code.src.quizGame.QuizGameController;
+import Code.src.wordleGame.WordleGameController;
+import Code.src.questionManager.QuestionManagerController;
 
 public class MainMenuControl {
-private MainMenuView view;
-public MainMenuControl(MainMenuView view) {
+    private MainMenuView view;
 
+    public MainMenuControl(MainMenuView view) {
+        this.view = view;
+    }
+
+    public MainMenuView getView() {
+        return view;
+    }
+
+    public void setView(MainMenuView view) {
+        this.view = view;
+    }
+
+    public void navigateToQuizGame() {
+        QuizGame quizGame = new QuizGame(this);
+        quizGame.display();
+        view.setVisible(false);
+    }
+
+    public void navigateToWordleGame() {
+        WordleGame wordleGame = new WordleGame(this);
+        wordleGame.display();
+        view.setVisible(false);
+    }
+
+    public void navigateToQuestionManager() {
+        QuestionManager questionManager = new QuestionManager(this);
+        questionManager.display();
+        view.setVisible(false);
+    }
 }
