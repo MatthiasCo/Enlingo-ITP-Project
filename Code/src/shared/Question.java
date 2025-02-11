33c37071f075f4addc27182d1aedbf09a9a666c2
Matt <127249327+MatthiasCo@users.noreply.github.com>
2025-02-11 12:14:00 +0100

    Feat: Question made ready for wordle

--- Code/src/shared/Question.java
+++ Code/src/shared/Question.java
@@ -8,21 +8,24 @@ public class Question<T> {
     private String text;
     private T[] answers;
     private boolean multiAnswer;
+    private boolean forWordle;
 
     public Question(int id, String text, T[] answers) {
         this.id = id;
         this.text = text;
         this.answers = answers;
         this.multiAnswer = true;
+        forWordle = false;
     }
 
     public Question(int id, String text, T answer, Class<T> type) {
         this.id = id;
         this.text = text;
-        // Create a new array of type T
         this.answers = (T[]) Array.newInstance(type, 1);
         this.answers[0] = answer;
         this.multiAnswer = false;
+        //is type is string and its 5 characters long
+        this.forWordle = type == Classes.STRING || answer.toString().length() == 5;
     }
 
     public int getId() {
@@ -43,7 +46,11 @@ public class Question<T> {
 
     @Override
     public String toString() {
-        return id + "," + text + "," + Arrays.toString(answers);
+        return "Question ID: " + id + "\n" +
+                "Text: " + text + "\n" +
+                "Answers: " + Arrays.toString(answers) + "\n" +
+                "MultiAnswer: " + multiAnswer + "\n" +
+                "For use in Wordle: " + forWordle;
     }
 
 }
